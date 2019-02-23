0262c7c85d09674eeddd9841804ed5d924940c61
ab siddik <absiddik96@gmail.com>
2019-02-23 10:37:36 +0600

    Many To Many Polymorphic relationship

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
+
+use App\Tag;
 use App\User;
 use App\Post;
 use App\Image;
+use App\Video;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,7 +24,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 | One To One Polymorphic relationship
 |--------------------------------------------------------------------------
 */
-// All user images
+// All users images
 Route::get('all-user-images', function() {
     return $users = User::with('image')->get();
     // foreach ($users as $user) {
@@ -40,7 +43,7 @@ Route::get('user/{id}/image', function($id) {
 | One To Many Polymorphic relationship
 |--------------------------------------------------------------------------
 */
-// All user post
+// All users post
 Route::get('all-user-posts', function() {
     return User::with('posts')->get();
 });
@@ -74,10 +77,40 @@ Route::get('image/{id}/ownership', function($id) {
     return $image->imageable;
 });
 
-// All image
+// All images
 Route::get('images/ownership', function() {
     $images = Image::all();
     foreach ($images as $image) {
-        echo $image->url.'---'.$image->imageable->name.'<br>';
+        echo $image->url.'---'.($image->imageable_type === 'App\User'?$image->imageable->name.'<br>':$image->imageable->title.'<br>');
     }
+});
+
+/*
+|--------------------------------------------------------------------------
+| Many To Many Polymorphic relationship
+|--------------------------------------------------------------------------
+*/
+// All posts tags
+Route::get('posts/tags', function() {
+    return Post::with('tags')->get();
+});
+
+// post tags
+Route::get('post/{id}/tags', function($id) {
+    return Post::with('tags')->findOrFail($id);
+});
+
+// All videos tags
+Route::get('videos/tags', function() {
+    return Video::with('tags')->get();
+});
+
+// video tags
+Route::get('video/{id}/tags', function($id) {
+    return Video::with('tags')->findOrFail($id);
+});
+
+// All tags owner
+Route::get('tags/owner', function() {
+    return Tag::with(['posts','videos'])->get();
 });
